326d411d4c8cccc57b1d1f6337448c5d11ddf9a7
pablovilchez <pablovilchez.r@gmail.com>
2023-04-21 13:11:34 +0200

    ok

--- 42/01Cursus/00-libft/libft.h
+++ 42/01Cursus/00-libft/libft.h
@@ -6,36 +6,29 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/04/17 23:57:25 by pvilchez          #+#    #+#             */
-/*   Updated: 2023/04/21 11:41:08 by pvilchez         ###   ########.fr       */
+/*   Updated: 2023/04/21 12:19:42 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef _LIBFT
-#define _LIBFT
+#ifndef LIBFT_H
+# define LIBFT_H
 
-#include <stdlib.h>
-
-typedef struct libft
-{
-	/* data */
-};
-
+# include <stdlib.h>
 
 void	ft_bzero(void *s, size_t n);
-int	ft_isalnum(char c);
-int	ft_isalpha(char c);
-int	ft_isascii(char c);
-int	ft_isdigit(char c);
-int	ft_isprint(char c);
+int		ft_isalnum(char c);
+int		ft_isalpha(char c);
+int		ft_isascii(char c);
+int		ft_isdigit(char c);
+int		ft_isprint(char c);
 void	*ft_memcpy(void *dest, const void *src, size_t n);
 void	*ft_memmove(void *str1, const void *str2, size_t n);
 void	*ft_memset(void *str, int c, unsigned int n);
 char	*ft_strchr(const char *str, int c);
 size_t	ft_strlcat(char *dst, char *src, size_t size);
 size_t	ft_strlcpy(char *dst, char *src, size_t size);
-int	ft_strlen(char *str);
-int	ft_tolower(int ch);
-int	ft_toupper(int ch);
+int		ft_strlen(char *str);
+int		ft_tolower(int ch);
+int		ft_toupper(int ch);
 
-#include "libft.h"
 #endif
