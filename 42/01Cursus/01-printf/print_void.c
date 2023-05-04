554bf77e9d708203332c6e1ce7e9b6dc4d6416e7
pablovilchez <pablovilchez.r@gmail.com>
2023-05-04 21:12:28 +0200

    ok

--- 42/01Cursus/01-printf/print_void.c
+++ 42/01Cursus/01-printf/print_void.c
@@ -6,20 +6,18 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/03 20:20:34 by pvilchez          #+#    #+#             */
-/*   Updated: 2023/05/04 19:17:34 by pvilchez         ###   ########.fr       */
+/*   Updated: 2023/05/04 21:12:28 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libftprintf.h"
-
-
 
 size_t	print_void(unsigned long long p)
 {
 	size_t				count;
 	unsigned long long	num;
 
-	
 	count = 0;
+
 	return (count);
 }
