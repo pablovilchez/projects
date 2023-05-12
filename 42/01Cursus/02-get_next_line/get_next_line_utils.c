83fd94864a4f9af692ffea65d065d6122d97fb49
Pol <pablovilchez.r@gmail.com>
2023-05-12 11:28:35 +0200

    ok

--- 42/01Cursus/02-get_next_line/get_next_line_utils.c
+++ 42/01Cursus/02-get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 15:13:47 by pvilchez          #+#    #+#             */
-/*   Updated: 2023/05/09 18:42:05 by pvilchez         ###   ########.fr       */
+/*   Updated: 2023/05/11 12:07:31 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,4 +20,24 @@ void	*ft_calloc(size_t number, size_t size)
 	if (c != NULL)
 		ft_memset(c, 0, number * size);
 	return (c);
+}
+
+ft_strlcpy(char *dst, char *src, size_t size)
+{
+	size_t	i;
+	size_t	c;
+
+	c = 0;
+	while (src[c])
+		c++;
+	i = 0;
+	if (size > 0)
+	{
+		while (src[i] && i < size - 1)
+		{
+			dst[i] = src[i];
+			i++;
+		}
+		dst[i] = '\0';
+	}
 }
