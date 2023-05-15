a8667b3a04db4782472f6194c0b5932ba412e840
pablovilchez <pablovilchez.r@gmail.com>
2023-05-15 22:38:30 +0200

    ok

--- 42/01Cursus/02-get_next_line/get_next_line.c
+++ 42/01Cursus/02-get_next_line/get_next_line.c
@@ -6,11 +6,27 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 15:01:42 by pvilchez          #+#    #+#             */
-/*   Updated: 2023/05/14 02:10:34 by pvilchez         ###   ########.fr       */
+/*   Updated: 2023/05/15 21:55:12 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "get_next_line.h"
+
+size_t	find_nl(char *str)
+{
+	size_t	i;
+	size_t	found;
+
+	i = 0;
+	found = 0;
+	while (str[i])
+	{
+		if (str[i] == '\n')
+			found = 1;
+		i++;
+	}
+	return (found);
+}
 
 char	*extra_data(char *static_str)
 {
@@ -18,64 +34,52 @@ char	*extra_data(char *static_str)
 	char	*aux;
 	size_t	len;
 
-	len = ft_strlen(static_str) - len_end(static_str);
+	str_aux = NULL;
+	if (static_str == NULL)
+		return (NULL);
+	len = ft_strlen(static_str) - count_nl(static_str);
 	if (len > 0)
 	{
-		str_aux = calloc(len + 1, sizeof(char));
-		aux = &static_str[len_end(static_str)];
-		str_aux = ft_memcpy(str_aux, aux, len);
-		free(static_str);
-		static_str = str_aux;
+		str_aux = (char *)ft_calloc(len + 1, sizeof(char));
+		if (str_aux == NULL)
+		{
+			free(static_str);
+			return (NULL);
+		}
+		aux = &static_str[count_nl(static_str)];
+		str_aux = (char *)ft_memcpy(str_aux, aux, len);
 	}
 	else
-	{
+		str_aux = static_str;
+	if (static_str != NULL && static_str[0] != '\0')
 		free(static_str);
-		static_str = ft_calloc(1, sizeof(char));
-	}
-	return (static_str);
+	return (str_aux);
 }
 
 char	*line_to_print(char *static_str, char *line)
 {
 	size_t	len;
 
-	len = len_end(static_str);
-	line = (char *)calloc(len + 1, sizeof(char));
+	if (find_nl(static_str) == 1)
+		len = count_nl(static_str);
+	else
+		len = ft_strlen(static_str);
+	line = (char *)ft_calloc(len + 1, sizeof(char));
 	if (line == NULL)
 		return (NULL);
-	line = ft_memcpy(line, static_str, len);
+	ft_memcpy(line, static_str, len);
 	line[len] = '\0';
 	return (line);
-}
-
-size_t	find_end(char *buffer)
-{
-	size_t	i;
-	size_t	found;
-
-	i = 0;
-	found = 0;
-	while (buffer[i] != '\0')
-	{
-		if (buffer[i] == '\n')
-			found = 1;
-		i++;
-	}
-	return (found);
 }
 
 char	*file_to_static(int fd, char *static_str)
 {
 	int		num_bytes;
 	char	*buffer;
-	size_t	new_line;
 
-	new_line = 0;
-	if (!static_str)
-		static_str = (char *)ft_calloc(1, sizeof(char));
 	num_bytes = 1;
 	buffer = (char *)ft_calloc((BUFFER_SIZE + 1), sizeof(char));
-	while (num_bytes > 0 && new_line == 0)
+	while (num_bytes > 0)
 	{
 		if (read(fd, 0, 0) == -1)
 		{
@@ -83,10 +87,14 @@ char	*file_to_static(int fd, char *static_str)
 			free(buffer);
 			return (NULL);
 		}
+		if (static_str)
+		{
+			if (find_nl(static_str) == 1)
+				break ;
+		}
 		num_bytes = read(fd, buffer, BUFFER_SIZE);
 		buffer[num_bytes] = '\0';
-		new_line = find_end(buffer);
-		static_str = ft_strjoin_endline(static_str, buffer);
+		static_str = ft_strjoin(static_str, buffer);
 	}
 	free(buffer);
 	return (static_str);
@@ -98,12 +106,14 @@ char	*get_next_line(int fd)
 	char		*line;
 
 	line = NULL;
-	static_str = NULL;
 	if (!(fd >= 0 && BUFFER_SIZE > 0) || read(fd, 0, 0) < 0)
 	{
-		free(static_str);
+		if (static_str != NULL)
+			free(static_str);
 		return (0);
 	}
+	if (!static_str)
+		static_str = (char *)ft_calloc(1, sizeof(char));
 	static_str = file_to_static(fd, static_str);
 	line = line_to_print(static_str, line);
 	static_str = extra_data(static_str);
--- 42/01Cursus/02-get_next_line/get_next_line_utils.c
+++ 42/01Cursus/02-get_next_line/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/07 15:13:47 by pvilchez          #+#    #+#             */
-/*   Updated: 2023/05/14 02:06:39 by pvilchez         ###   ########.fr       */
+/*   Updated: 2023/05/15 20:01:28 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,7 +17,7 @@ void	*ft_calloc(size_t number, size_t size)
 	unsigned char	*c;
 	size_t			i;
 
-	c = malloc(number * size);
+	c = (unsigned char *)malloc(number * size);
 	if (c != NULL)
 	{
 		i = 0;
@@ -29,6 +29,31 @@ void	*ft_calloc(size_t number, size_t size)
 		return (c);
 	}
 	return (0);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*dest;
+
+	if (s1 == NULL || s2 == NULL || s2[0] == '\0')
+		return (NULL);
+	dest = (char *)ft_calloc((ft_strlen(s1) + ft_strlen(s2) + 1), sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	ft_memcpy(dest, s1, ft_strlen(s1));
+	ft_memcpy(&dest[ft_strlen(s1)], s2, ft_strlen(s2));
+	free(s1);
+	return (dest);
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	i;
+
+	i = 0;
+	while (*(str + i) != '\0')
+		i++;
+	return (i);
 }
 
 void	*ft_memcpy(void *dest, const void *src, size_t n)
@@ -50,39 +75,14 @@ void	*ft_memcpy(void *dest, const void *src, size_t n)
 	return (d);
 }
 
-char	*ft_strjoin_endline(char *str, char *buffer)
-{
-	char	*dest;
-
-	if (str == NULL || buffer == NULL)
-		return (NULL);
-	dest = ft_calloc((len_end(str) + len_end(buffer) + 1), sizeof(char));
-	if (dest == NULL)
-		return (NULL);
-	dest = ft_memcpy(dest, str, len_end(str));
-	ft_memcpy(&dest[len_end(str)], buffer, len_end(buffer));
-	free(str);
-	return (dest);
-}
-
-size_t	len_end(const char *str)
+size_t	count_nl(char *str)
 {
 	size_t	i;
 
 	i = 0;
-	while (*(str + i) != '\0' && *(str + i) != '\n')
+	while (str[i] != '\0' && str[i] != '\n')
 		i++;
-	if (*(str + i) == '\n')
-		i++;
-	return (i);
-}
-
-size_t	ft_strlen(const char *str)
-{
-	size_t	i;
-
-	i = 0;
-	while (*(str + i) != '\0')
+	if (str[i] == '\n')
 		i++;
 	return (i);
 }
