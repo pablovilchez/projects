98a61131885dd641b57e6d1532feb400c4ad93c8
Pol <pablovilchez.r@gmail.com>
2023-05-17 20:19:02 +0200

    ok

---
+++ 42/01Cursus/04-push_swap/libftpushswap.h
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   libftpushswap.h                                    :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/05/17 20:11:31 by pvilchez          #+#    #+#             */
+/*   Updated: 2023/05/17 20:13:03 by pvilchez         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef LIBFTPUSHSWAP_H
+# define LIBFTPUSHSWAP_H
+
+# include <stdlib.h>
+# include <unistd.h>
+# include <stdarg.h>
+# include "libft/libft.h"
+
+
+#endif
