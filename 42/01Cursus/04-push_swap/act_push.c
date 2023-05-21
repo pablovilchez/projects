a81620aacd51fa0b30010f03bede6e1110d9a1d9
Pol <pablovilchez.r@gmail.com>
2023-05-21 23:34:59 +0200

    ok

--- 42/01Cursus/04-push_swap/act_push.c
+++ 42/01Cursus/04-push_swap/act_push.c
@@ -1,33 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftpushswap.h                                    :+:      :+:    :+:   */
+/*   act_push.c                                         :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/17 20:11:31 by pvilchez          #+#    #+#             */
-/*   Updated: 2023/05/21 23:34:49 by pvilchez         ###   ########.fr       */
+/*   Created: 2023/05/21 22:32:12 by pvilchez          #+#    #+#             */
+/*   Updated: 2023/05/21 23:34:33 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPUSHSWAP_H
-# define LIBFTPUSHSWAP_H
+#include "libftpushswap.h"
 
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdarg.h>
-# include "libft/libft.h"
-
-typedef struct n_list
+void	act_push(t_node **lst)
 {
-	int				num;
-	struct n_list	*next;
-}					t_node;
-void	push_swap(t_node **lst_a, t_node **lst_b);
-void	print_lst(t_node **lst);
-t_node	*lst_new(int num);
-int	lst_size(t_node *lst);
-t_node	*lst_last(t_node *lst);
-
-
-#endif
+	
+}
--- 42/01Cursus/04-push_swap/act_rev.c
+++ 42/01Cursus/04-push_swap/act_rev.c
@@ -1,33 +1,13 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   libftpushswap.h                                    :+:      :+:    :+:   */
+/*   act_rev.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/17 20:11:31 by pvilchez          #+#    #+#             */
-/*   Updated: 2023/05/21 23:34:49 by pvilchez         ###   ########.fr       */
+/*   Created: 2023/05/21 22:32:33 by pvilchez          #+#    #+#             */
+/*   Updated: 2023/05/21 23:34:37 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef LIBFTPUSHSWAP_H
-# define LIBFTPUSHSWAP_H
-
-# include <stdlib.h>
-# include <unistd.h>
-# include <stdarg.h>
-# include "libft/libft.h"
-
-typedef struct n_list
-{
-	int				num;
-	struct n_list	*next;
-}					t_node;
-void	push_swap(t_node **lst_a, t_node **lst_b);
-void	print_lst(t_node **lst);
-t_node	*lst_new(int num);
-int	lst_size(t_node *lst);
-t_node	*lst_last(t_node *lst);
-
-
-#endif
+#include "libftpushswap.h"
