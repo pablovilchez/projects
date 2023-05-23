b4d186e8d9fe9dc217106f954c48215303fa0f10
Pol <pablovilchez.r@gmail.com>
2023-05-23 22:10:46 +0200

    ok

--- 42/01Cursus/04-push_swap/push_swap.c
+++ 42/01Cursus/04-push_swap/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: pvilchez <pvilchez@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/21 23:18:02 by pvilchez          #+#    #+#             */
-/*   Updated: 2023/05/23 21:37:08 by pvilchez         ###   ########.fr       */
+/*   Updated: 2023/05/23 21:56:06 by pvilchez         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,7 @@ void	push_swap(t_node **lst_a, t_node **lst_b)
 	printf("\n");
 	rotate(lst_a);
 	lst_print(lst_a);
+	printf("\n");
 	swap(lst_a);
 	lst_print(lst_a);
 	rotate(lst_b);
